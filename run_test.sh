4bc8fa0899d722968d4b9d360095f39f9ea293e7
syntrust <lundeng@quarkchain.org>
2024-04-08 15:43:43 +0800

    support story protocol testnet

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 ./server \
-    -setNS 3334,w3q,W3NS,0xD379B91ac6a93AF106802EB076d16A54E3519CED  \
+    -setNS 3334,w3q,w3ns,0xD379B91ac6a93AF106802EB076d16A54E3519CED  \
     -setChain 3334,w3q-g,https://galileo.web3q.io:8545  \
     -setNSChain w3q,3334 \
     -setNSChain eth,5 \
