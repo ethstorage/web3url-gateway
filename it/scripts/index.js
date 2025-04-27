7bb50113a96eba17dfb91daf29105749bd68d9d0
DL <lundeng@quarkchain.org>
2025-04-27 18:06:06 +0800

    Workflow to check web3 links part 2: support new links (#54)
    * support new links
    
    * fix secrets
    
    * fix pk
    
    * refactor
    
    * debug
    
    * minor
    
    * fix timeout
    
    * debug
    
    * fix timeout
    
    * debug

--- it/scripts/index.js
+++ it/scripts/index.js
@@ -1,12 +1,24 @@
 import { links } from '../data/links.mjs';
+import { addLinks } from './add-links.mjs';
 import { checkAllLinks } from './check-links.mjs';
 
 async function run() {
     const results = await checkAllLinks(links);
-    if (results.success) {
-        console.log('All links are OK');
+    if (results.failures.length > 0) {
+        console.log('Failed links: \n', results.failures.map(f => `- ${f.url}: ${f.error}`).join('\n'));
     } else {
-        console.log('results', JSON.stringify(results.failures, null, 2));
-    }}
-    
-run();
+        console.log('All links are OK: totally', results.all.length);
+    }
+
+    const newLinks = await addLinks();
+    console.log('New links', newLinks);
+
+    const newResults = await checkAllLinks(newLinks);
+    if (newResults.failures.length > 0) {
+        console.log('Failed links: \n', newResults.failures.map(f => `- ${f.url}: ${f.error}`).join('\n'));
+    } else {
+        console.log('New links are OK');
+    }
+}
+
+await run();
