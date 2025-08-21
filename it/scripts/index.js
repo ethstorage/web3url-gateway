f9dea4bb0b34d14740a0464669fc2722d4713e36
syntrust <lundeng@quarkchain.org>
2025-08-21 19:10:00 +0800

    minor

--- it/scripts/index.js
+++ it/scripts/index.js
@@ -1,13 +1,14 @@
+import { links } from '../data/links.mjs';
 import { addLinks } from './add-links.mjs';
 import { checkAllLinks } from './check-links.mjs';
 
 async function run() {
-    // const failures = await checkAllLinks(links);
-    // if (failures.size > 0) {
-    //     console.log('Failed links: \n', Array.from(failures, ([key, value]) => `${key} -> ${value}`).join('\n'));
-    // } else {
-    //     console.log('All links are OK: totally', links.length);
-    // }
+    const failures = await checkAllLinks(links);
+    if (failures.size > 0) {
+        console.log('Failed links: \n', Array.from(failures, ([key, value]) => `${key} -> ${value}`).join('\n'));
+    } else {
+        console.log('All links are OK: totally', links.length);
+    }
 
     const newLinks = await addLinks();
     console.log('New links', newLinks);
